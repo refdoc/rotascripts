687ae40f2c2549a14984ca21db88e1a5ad50bcde
Peter von Kaehne <refdoc@crosswire.org>
2018-10-20 20:51:50 +0100

    moved to better name for script

--- rota2ics.pl
+++ rota2ics.pl
@@ -1,5 +1,5 @@
 #!/usr/bin/perl
-# This script takes a OOH rota in CVS format and transforms it into individual ical files, one for each doctor. 
+# This script takes a OOH rota in CVS format and transforms it into individual ics files, one for each doctor. 
 # The format of the CSV file must be 
 # - date in DD/MM/YYYY, weekday in short form, name of doctor, BH for Bank holiday or N for normal, each entry separated by a comma
 # the output are a bunch of ical files for each doctor one . 
@@ -9,7 +9,7 @@
 
 sub main {
     if (scalar(@ARGV) < 1) {
-        print "\nrota2ical.pl -- - provides ical files from a provided CSV file containing dates, day of week, name of doctor and info on BH or else.\n";
+        print "\nrota2ics.pl -- - provides ical files from a provided CSV file containing dates, day of week, name of doctor and info on BH or else.\n";
         print "Syntax: rota2ical.pl csvfile [-d output-directory>]";
         print "- Arguments in braces < > are required. Arguments in brackets [ ] are optional.\n";
         print "- If no -d option is specified <STDOUT> is used.\n";
