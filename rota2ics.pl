0a3e1e19bd64c39a055b7a9b6f979b8eee36036a
Refdoc <refdoc@crosswire.org>
2022-10-21 19:27:18 +0100

    bug fix in UID date

--- rota2ics.pl
+++ rota2ics.pl
@@ -4,8 +4,8 @@
 # - date in DD/MM/YYYY, weekday in short form, name of doctor, BH for Bank holiday or N for normal, each entry separated by a comma
 # the output are a bunch of ical files for each doctor one . 
 # The script is copyrighted by Peter von Kaehne and is published under the GPL version 2 or later
-#use strict;
-#use warnings;
+use strict;
+use warnings;
 
 sub main {
     if (scalar(@ARGV) < 1) {
@@ -54,20 +54,19 @@ sub main {
         
         my $i=0;
                         
-        foreach $line (@lines) {
+        foreach my $line (@lines) {
             
             
             my @line = split(',', $line);
             my $shiftPattern = $line[3];
-            if ($_ eq @line[2]) {
+            if ($_ eq $line[2]) {
                 
                 $line[1] =~s/^(\d{2})\/(\d{2})\/(\d{2})/$2\/$1\/$3/;
                 my $startTime = `date -I'seconds' -d "$line[1] $shiftStart{$shiftPattern}" `;
                 chomp($startTime);
                 
                 print OUTF "BEGIN:VEVENT\n";
-                print OUTF "UID:$line[2]-".$i."-".`date -I -d "$shiftStart"`;
-                $i++;
+                print OUTF "UID:$line[2]-".`date -I -d "$startTime"`;
                 print OUTF "ORGANISER:XYZ\n";
                 print OUTF "DTSTAMP:".`date "+%G%m%d"`;
                 print OUTF "SUMMARY:OOH Shift\n";
